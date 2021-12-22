acbd827e2b0d994c61640a3bee9e39cf35dd1bc1
Venkatesan Krishnamurthy <venkatesan1996@gmail.com>
2021-12-22 11:54:18 +0100

    myname function added

--- test_1.py
+++ test_1.py
@@ -1,4 +1,6 @@
 import numpy as np
 
-print(np.random.randint(10, 50, size=(4, 4)))
-print(np.random.randint(10, 50, size=(3, 3)))
+
+# function that welcomes you
+def myname(name):
+    print("hello" + name)
